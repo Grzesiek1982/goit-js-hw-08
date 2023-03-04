db9cc25c43f2b92cb09512f20461fcc7937e739d
Grzesiek1982 <grzesieg1982@interia.pl>
2023-03-04 17:08:14 +0100

    zadanie 08-01 final

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,36 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from './gallery-items.js';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = galleryItems;
+const divGallery = document.querySelector('div.gallery');
+
+const markup = gallery
+  .map(({ preview, original, description }) => {
+    return `<a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>`;
+  })
+  .join('');
+
+divGallery.insertAdjacentHTML('beforeend', markup);
+
+divGallery.addEventListener('click', urlImage);
+
+function urlImage(event) {
+  event.preventDefault();
+  const url = event.target.dataset.source;
+  if (!url) return;
+  console.log(url);
+  const instance = basicLightbox.create(`
+    <img src="${url}" width="800" height="600">
+`);
+  instance.show(() => console.log('lighboxvisible'));
+}
